1eedf4e6e196aafca95dc10b20cc003f74d4dfc6
Heloisa Natalia Ramos <152300068+helooramos@users.noreply.github.com>
2024-09-25 18:54:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Moana',
+    'O que é "Moana"?',
+    'É um filme de animação da Disney, lançado em 2016, que conta a história de uma jovem polinésia que se embarca em uma jornada para salvar sua ilha.'
+)
+
+criaCartao(
+    'Moana',
+    'Quem são os personagens principais?',
+    'Os personagens principais são Moana, uma corajosa jovem navegadora, e Maui, um semideus transformador, dublado por Dwayne Johnson.'
+)
+
+criaCartao(
+    'Moana',
+    'Qual é a canção mais famosa do filme?',
+    ' A canção mais famosa é "How Far I`ll Go," interpretada por Auli`i Cravalho, que dá voz a Moana. A música fala sobre a busca de Moana por seu verdadeiro propósito.'
+)
+
+criaCartao(
+    'Moana',
+    'Qual é a mensagem central do filme?',
+    'O filme aborda temas de identidade, coragem e a importância de seguir seu coração, além de valorizar a cultura e as tradições polinésias.'
+)
